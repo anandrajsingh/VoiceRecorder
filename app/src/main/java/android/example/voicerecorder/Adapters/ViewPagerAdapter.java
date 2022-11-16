5ba320290531f10074c6c1ff5baf63b05ea1ef30
Anand Raj Singh <singhpariharanandraj@gmail.com>
2022-11-16 18:27:47 +0530

    Added ViewPagerAdapter

---
+++ app/src/main/java/android/example/voicerecorder/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package android.example.voicerecorder.Adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragments = new ArrayList<>();
+    private final ArrayList<String> fragTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragments.add(fragment);
+        fragTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragTitle.get(position);
+    }
+
+
+}
--- app/src/main/java/android/example/voicerecorder/MainActivity.java
+++ app/src/main/java/android/example/voicerecorder/MainActivity.java
@@ -1,14 +1,35 @@
 package android.example.voicerecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.example.voicerecorder.Adapters.ViewPagerAdapter;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        setSupportActionBar(toolbar);
+
+    }
+
+    private void setupViewpager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        
     }
 }
